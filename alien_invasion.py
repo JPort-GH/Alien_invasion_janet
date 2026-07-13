549df65053d654c9c8b6896bbe68e926ce35dfc8
jport-gh <janet4portillo@gmail.com>
2026-07-13 01:43:01 -0400

    Implement: Created AlienInvasion class and initialized pygame display

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,31 @@
 # Name: Janet Portillo
 # GitHub: JPort-GH
-# 07.13.2026
+# Date: 07.13.2026
 # Origin (0,0) is the top-left of the screen. X increases to the right, Y increases downward.
 
-import sys
 import pygame
+from settings import Settings
 
-if __name__ == '__main__':
-    pass
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        pygame.init()  # Initialize all pygame modules
+        self.settings = Settings()
+
+        # Create the game window
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Fill the screen with background color
+            self.screen.fill(self.settings.bg_color)
+            pygame.display.flip()  # Update the display each loop
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()
