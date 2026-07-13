6efb9ebd5c67a16de021cae90b263f96d5a87236
jport-gh <janet4portillo@gmail.com>
2026-07-13 00:56:35 -0400

    Added header and coordinate comment

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,8 @@
+# Name: Janet Portillo
+# GitHub: JPort-GH
+# 07.13.2026
+# Origin (0,0) is the top-left of the screen. X increases to the right, Y increases downward.
+
 import sys
 import pygame
 
